d3828727a447c891de40434fd3c1c3f7a74eb60a
George <george.e.correa@gmail.com>
2017-04-13 18:03:35 -0400

    Added first yellow view to project.

--- RemakeSampleProgUI/RemakeSampleProgUI/RemakeTheApp.swift
+++ RemakeSampleProgUI/RemakeSampleProgUI/RemakeTheApp.swift
@@ -22,6 +22,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         rootVC.view.backgroundColor = .black
         self.window = UIWindow(frame: UIScreen.main.bounds)
         rootVC.view.frame = (window?.frame)!
+        self.window?.rootViewController = rootVC
+        self.window?.makeKeyAndVisible()
         
         // Override point for customization after application launch.
         return true
@@ -53,13 +55,31 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 class YellowView: UIView {
     
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override required init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .yellow
+        self.alpha = 0.7
+        self.layer.cornerRadius = 5
+        self.layer.shadowColor = UIColor.white.cgColor
+        self.layer.shadowOffset = CGSize(width: 5.0, height: 5.0)
+        self.layer.shadowRadius = 5
+        self.layer.shadowOpacity = 0.5
+    }
 }
 
 class ViewController: UIViewController {
     
+    var myView = YellowView(frame: CGRect(x: 20, y: 35, width: 130, height: 50))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.addSubview(myView)
     }
     
     override func didReceiveMemoryWarning() {
