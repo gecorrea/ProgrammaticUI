f8463cb5563bf8ccf8f07f1980bd65756b566e69
George <george.e.correa@gmail.com>
2017-04-13 16:28:42 -0400

    Started remaking the sample project.

---
+++ RemakeSampleProgUI/RemakeSampleProgUI/ViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ViewController.swift
+//  RemakeSampleProgUI
+//
+//  Created by Aditya Narayan on 4/13/17.
+//  Copyright © 2017 George Correa. All rights reserved.
+//
+
+import UIKit
+
+
+
